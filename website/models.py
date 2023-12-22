582c9fee4a7aa9f45ac297b3a63cad24852588ec
Satyam Kumar <satyamk.verve.com>
2023-12-22 18:59:32 +0530

    CRM website

--- website/models.py
+++ website/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    models.DateField(auto_now=True, auto_now_add=True)
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    emamil=models.EmailField( max_length=254)
+    phone=models.CharField(max_length=12)
+    address=models.CharField(max_length=50)
+    city=models.CharField( max_length=50)
+    state=models.CharField( max_length=50)
+    zip_code=models.CharField( max_length=50)
+    
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
+    
+    
+    
+   
+   
+   
+   
+   
+   
+   
+    
+    
+    
+
+
+
+
--- website/views.py
+++ website/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import SignUpForm
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You have been logged in!")
+                return redirect('home')
+        else:
+            messages.error(request, "Invalid credentials. Please try again.")
+            return redirect('home')
+    else:
+        # If the request method is not POST or the authentication failed, render the form again
+        form = AuthenticationForm()
+        return render(request, 'home.html', {'form': form}) #passing the form coz when we go to home page then we can do something with the input fields
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('home')
+    
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password) #why password not password1!!, coz we took password1 and signed it to variable called password
+            login(request, user)
+            messages.success(request, "You have successfully register")
+            return redirect('home')
+    else:
+        form= SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
